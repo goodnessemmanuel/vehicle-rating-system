86ed53255ba0cbc2f8802efdd49ba32677d22e9f
Oche Ejeh <oeejeh@unionbankng.com>
2022-06-12 12:16:48 +0200

    add logic to rate and display vehicle according to rating

--- vehicle.go
+++ vehicle.go
@@ -10,8 +10,7 @@ import (
 )
 
 //vehicle interface
-type vehicle interface {
-}
+type vehicle interface {}
 
 type car struct {
 	model       string
@@ -78,7 +77,6 @@ func init() {
 		truck{"RAM1500", "Dodge", "Truck"}}
 
 	vehicleResult = make(map[string]feedbackResult)
-	fmt.Println(inventory)
 
 }
 
@@ -88,6 +86,18 @@ func main() {
 	generateRating()
 
 	// Print ratings for the different vehicles
+	for _, veh := range inventory{
+		switch v := veh.(type){
+			case car:
+				v.carDetails()
+			case bike:
+				v.bikeDetails()
+			case truck:
+				v.truckDetails()
+			default:
+				fmt.Printf("Are you sure this Vehicle Type exists")
+		}
+	}
 }
 
 
@@ -142,5 +152,33 @@ func generateRating(){
 		}
 	    vehicleResult[v.Name] = vehResult
 	}
+}
+
+func showRating(model string){
+	ratingFound := false
+	for m, r := range vehicleResult{
+		if m == model{
+			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral);
+			ratingFound = true;
+		}
+	}
+	if !ratingFound {
+		fmt.Printf("No rating for this vehicle");
+	}
+}
+
+func(c *car) carDetails(){
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car" , c.make, c.model);
+	showRating(c.model)
+}
+
+func(b *bike)  bikeDetails(){
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike" , b.make, b.model);
+	showRating(b.model)
+}
+
+func(t *truck)  truckDetails(){
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Truck" , t.make, t.model);
+	showRating(t.model)
 }
 
